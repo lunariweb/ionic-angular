c2947e85311d4a7a12d4cec85286294dede7e927
Nimesha <nimeshadls8@gmail.com>
2020-12-29 19:43:56 +0530

    created the new ionic with angular project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text = 'Default starting text';
+  onChangeText () {
+    this.text = 'Changed!';
+  }
 
   constructor() {}
 
